fba17b71b6cf06579dbde1e8e3b6b9dae1a07cbe
jpb <jpb44173@gmail.com>
2026-07-07 12:46:41 +0100

    veux: Use older clang version
    Required for successful boot to recovery on CP2A
    
    Added as part of guidance from pdx225 CP2A bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,7 +93,7 @@ BOARD_KERNEL_CMDLINE := \
 TARGET_KERNEL_CONFIG := vendor/veux-qgki_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/veux
 TARGET_KERNEL_NO_GCC := true
-
+TARGET_KERNEL_CLANG_VERSION := r563880c
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := peux|veux
